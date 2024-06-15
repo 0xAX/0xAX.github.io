675b2b75b4ddedf0a3651d92ab797b38e6c62964
Alexander Kuleshov <kuleshovmail@gmail.com>
2024-06-15 17:12:01 +0500

    Updates

--- assets/js/0d9b5f8b.8435f171.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7565],{790:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/0xAX.github.io/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
